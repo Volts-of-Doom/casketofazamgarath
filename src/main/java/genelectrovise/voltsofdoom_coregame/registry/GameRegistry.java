778f4364e7d26837f0ddefec51f3a44fbd63121c
GenElectrovise <sanchez.pamelos@outlook.com>
2020-03-31 18:09:45 +0100

    Now parses jsons into levels and adventures!

--- src/main/java/genelectrovise/voltsofdoom_coregame/registry/GameRegistry.java
+++ src/main/java/genelectrovise/voltsofdoom_coregame/registry/GameRegistry.java
@@ -3,21 +3,31 @@ package genelectrovise.voltsofdoom_coregame.registry;
 import genelectrovise.voltsofdoom_coregame.main.VoltsOfDoomGame;
 import genelectrovise.voltsofdoom_coresystem.annotation.GenericRegistryHolder;
 import genelectrovise.voltsofdoom_coresystem.annotation.Mod;
+import genelectrovise.voltsofdoom_coresystem.log.VODLog4J;
 import genelectrovise.voltsofdoom_coresystem.main.ModItemTest;
 import genelectrovise.voltsofdoom_coresystem.registry.generic.GenericRegistry;
 import genelectrovise.voltsofdoom_coresystem.registry.mod.RegistryObject;
-import genelectrovise.voltsofdoom_coresystem.util.VODLog;
+import genelectrovise.voltsofdoom_coresystem.tile.Tile;
 
 @GenericRegistryHolder(holderId = VoltsOfDoomGame.MODID)
 public class GameRegistry {
 	public static final GenericRegistry<ModItemTest> MOD_ITEM_TEST_REGISTRY = new GenericRegistry<ModItemTest>(
 			ModItemTest.class, VoltsOfDoomGame.class.getAnnotation(Mod.class));
+	public static final GenericRegistry<Tile> TILE_REGISTRY = new GenericRegistry<Tile>(Tile.class,
+			VoltsOfDoomGame.class.getAnnotation(Mod.class));
 
 	public static final RegistryObject<ModItemTest> TEST_OBJ = MOD_ITEM_TEST_REGISTRY.register("test_obj",
-			new RegistryObject<ModItemTest>());;
+			new RegistryObject<ModItemTest>(null));
+	public static final RegistryObject<Tile> TILE_COBBLE = TILE_REGISTRY.register("tilecobble",
+			new RegistryObject<Tile>(null));;
+	public static final RegistryObject<Tile> TILE_WOODLOG = TILE_REGISTRY.register("tilewoodlog",
+			new RegistryObject<Tile>(null));;
+	public static final RegistryObject<Tile> TILE_DOOR = TILE_REGISTRY.register("door",
+			new RegistryObject<Tile>(null));;
 
 	public GameRegistry() {
-		VODLog.LOGGER.status("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
+		VODLog4J.LOGGER.status("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
 		MOD_ITEM_TEST_REGISTRY.addToGenericRegisteringQueue((Mod) VoltsOfDoomGame.instance);
+		TILE_REGISTRY.addToGenericRegisteringQueue((Mod) VoltsOfDoomGame.instance);
 	}
 }
