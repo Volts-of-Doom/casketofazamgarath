d7c199b7342cb027f32bb18360218e035d6dceb7
GenElectrovise <sanchez.pamelos@outlook.com>
2020-04-01 11:56:36 +0100

    Json reading updates

--- src/main/java/genelectrovise/voltsofdoom_coregame/registry/GameRegistry.java
+++ src/main/java/genelectrovise/voltsofdoom_coregame/registry/GameRegistry.java
@@ -3,6 +3,7 @@ package genelectrovise.voltsofdoom_coregame.registry;
 import genelectrovise.voltsofdoom_coregame.main.VoltsOfDoomGame;
 import genelectrovise.voltsofdoom_coresystem.annotation.GenericRegistryHolder;
 import genelectrovise.voltsofdoom_coresystem.annotation.Mod;
+import genelectrovise.voltsofdoom_coresystem.entity.Entity;
 import genelectrovise.voltsofdoom_coresystem.log.VODLog4J;
 import genelectrovise.voltsofdoom_coresystem.main.ModItemTest;
 import genelectrovise.voltsofdoom_coresystem.registry.generic.GenericRegistry;
@@ -15,6 +16,8 @@ public class GameRegistry {
 			ModItemTest.class, VoltsOfDoomGame.class.getAnnotation(Mod.class));
 	public static final GenericRegistry<Tile> TILE_REGISTRY = new GenericRegistry<Tile>(Tile.class,
 			VoltsOfDoomGame.class.getAnnotation(Mod.class));
+	public static final GenericRegistry<Entity> ENTITY_REGISTRY = new GenericRegistry<Entity>(Entity.class,
+			VoltsOfDoomGame.class.getAnnotation(Mod.class));
 
 	public static final RegistryObject<ModItemTest> TEST_OBJ = MOD_ITEM_TEST_REGISTRY.register("test_obj",
 			new RegistryObject<ModItemTest>(null));
@@ -25,9 +28,13 @@ public class GameRegistry {
 	public static final RegistryObject<Tile> TILE_DOOR = TILE_REGISTRY.register("door",
 			new RegistryObject<Tile>(null));;
 
+	public static final RegistryObject<Entity> ENTITY_BLOB = ENTITY_REGISTRY.register("entityblob",
+			new RegistryObject<Entity>(null));;
+
 	public GameRegistry() {
 		VODLog4J.LOGGER.status("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
 		MOD_ITEM_TEST_REGISTRY.addToGenericRegisteringQueue((Mod) VoltsOfDoomGame.instance);
 		TILE_REGISTRY.addToGenericRegisteringQueue((Mod) VoltsOfDoomGame.instance);
+		ENTITY_REGISTRY.addToGenericRegisteringQueue((Mod) VoltsOfDoomGame.instance);
 	}
 }
