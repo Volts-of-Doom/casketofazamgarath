9a339bf357c5d84c7f6c043f27bd3306d573f63c
GenElectrovise <sanchez.pamelos@outlook.com>
2020-07-11 12:39:44 +0100

    Any small compatability changes for synchronicity with coresystem rewrite

--- src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
+++ src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
@@ -1,16 +1,41 @@
 package vision.voltsofdoom.coregame.registry;
 
+import vision.voltsofdoom.coregame.main.VoltsOfDoomGame;
+import vision.voltsofdoom.coresystem.loading.registry.RegistryObjectRetriever;
+import vision.voltsofdoom.coresystem.loading.registry.RegistryTypes;
+import vision.voltsofdoom.coresystem.loading.registry.TypeRegistry;
+import vision.voltsofdoom.coresystem.loading.resource.ResourceLocation;
+import vision.voltsofdoom.coresystem.play.entity.Entity;
+import vision.voltsofdoom.coresystem.play.tile.Tile;
 import vision.voltsofdoom.coresystem.universal.band_wagon.Stowaway;
 import vision.voltsofdoom.coresystem.universal.event.LoadingEvent;
+import vision.voltsofdoom.coresystem.universal.event.RegistryEvent;
 import vision.voltsofdoom.coresystem.universal.log.VODLog4J;
 
 @Stowaway
 public class GameRegistry {
 
+	public static TypeRegistry<Tile> TILES;
+	public static TypeRegistry<Entity> ENTITIES;
+
+	public static RegistryObjectRetriever<Tile> test_tile;
+
 	public GameRegistry() {
 		VODLog4J.LOGGER.status("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
-		VODLog4J.LOGGER.error("GameRegistry depopulated!");
 	}
+
+	@Stowaway
+	private static void createAndSubmitTypeRegistries(RegistryEvent.CreateAndSubmitTypeRegistriesEvent event) {
+		TILES = new TypeRegistry<Tile>(new ResourceLocation(VoltsOfDoomGame.MODID, "tiles"), RegistryTypes.TILES);
+		ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(VoltsOfDoomGame.MODID, "entities"), RegistryTypes.ENTITIES);
+	}
+
+	@Stowaway
+	private static void populateTypeRegistriesEventListener(RegistryEvent.PopulateTypeRegistriesEvent event) {
+		test_tile = TILES.register(new ResourceLocation(VoltsOfDoomGame.MODID, "test_tile"), () -> new Tile(Tile.Archetype.COBBLE));
+	}
+
+	// DEBUG
 
 	public static void typeStowawayTestObject(Object notAnEvent) {
 
