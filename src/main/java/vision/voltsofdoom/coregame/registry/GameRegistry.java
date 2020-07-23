aadc086d57bfb62cf62d7b5765ff492664a8dbdc
GenElectrovise <sanchez.pamelos@outlook.com>
2020-07-23 12:53:44 +0100

    Testing additions for merging registries

--- src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
+++ src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
@@ -2,6 +2,7 @@ package vision.voltsofdoom.coregame.registry;
 
 import vision.voltsofdoom.coregame.main.VoltsOfDoomGame;
 import vision.voltsofdoom.coresystem.loading.registry.RegistryObjectRetriever;
+import vision.voltsofdoom.coresystem.loading.registry.RegistryType;
 import vision.voltsofdoom.coresystem.loading.registry.RegistryTypes;
 import vision.voltsofdoom.coresystem.loading.registry.TypeRegistry;
 import vision.voltsofdoom.coresystem.loading.resource.ResourceLocation;
@@ -15,24 +16,50 @@ import vision.voltsofdoom.coresystem.universal.log.VODLog4J;
 @Stowaway
 public class GameRegistry {
 
+	public static RegistryType<Tile> TILE_SECOND_TYPE;
+
+	public static TypeRegistry<Tile> TILE_SECOND;
+	public static TypeRegistry<Entity> ENTITY_SECOND_TO_BE_MERGED;
 	public static TypeRegistry<Tile> TILES;
 	public static TypeRegistry<Entity> ENTITIES;
 
 	public static RegistryObjectRetriever<Tile> test_tile;
+	public static RegistryObjectRetriever<Tile> test_tile_second;
+	public static RegistryObjectRetriever<Entity> test_entity;
+	public static RegistryObjectRetriever<Entity> test_entity_merged;
 
 	public GameRegistry() {
 		VODLog4J.LOGGER.status("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
 	}
 
 	@Stowaway
-	private static void createAndSubmitTypeRegistries(RegistryEvent.CreateAndSubmitTypeRegistriesEvent event) {
+	private static void createRegistryTypes(RegistryEvent.CreateRegistryTypesEvent event) {
+		VODLog4J.LOGGER.info("Creating RegistryTypes");
+		TILE_SECOND_TYPE = event.createRegistryType(new ResourceLocation(VoltsOfDoomGame.MODID, "tile_second"), Tile.class);
+	}
+
+	@Stowaway
+	private static void createAndSubmitTypeRegistries(RegistryEvent.CreateAndSubmitRegistriesEvent event) {
+		VODLog4J.LOGGER.info("Creating and submitting TypeRegistries");
 		TILES = new TypeRegistry<Tile>(new ResourceLocation(VoltsOfDoomGame.MODID, "tiles"), RegistryTypes.TILES);
 		ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(VoltsOfDoomGame.MODID, "entities"), RegistryTypes.ENTITIES);
+		TILE_SECOND = new TypeRegistry<Tile>(new ResourceLocation(VoltsOfDoomGame.MODID, "tile_second"), TILE_SECOND_TYPE);
+		ENTITY_SECOND_TO_BE_MERGED = new TypeRegistry<Entity>(new ResourceLocation(VoltsOfDoomGame.MODID, "entity_second_to_be_merged"), RegistryTypes.ENTITIES);
+		event.submit(TILES);
+		event.submit(ENTITIES);
+		event.submit(TILE_SECOND);
+		event.submit(ENTITY_SECOND_TO_BE_MERGED);
 	}
 
 	@Stowaway
 	private static void populateTypeRegistriesEventListener(RegistryEvent.PopulateTypeRegistriesEvent event) {
+		VODLog4J.LOGGER.info("Populating TypeRegistries");
 		test_tile = TILES.register(new ResourceLocation(VoltsOfDoomGame.MODID, "test_tile"), () -> new Tile(Tile.Archetype.COBBLE));
+
+		test_tile_second = TILE_SECOND.register(new ResourceLocation(VoltsOfDoomGame.MODID, "tile_second"), () -> new Tile(Tile.Archetype.WOODLOG));
+
+		test_entity = ENTITIES.register(new ResourceLocation(VoltsOfDoomGame.MODID, "entity"), () -> new Entity(Entity.Archetype.ENTITYBLOB));
+		test_entity_merged = ENTITY_SECOND_TO_BE_MERGED.register(new ResourceLocation(VoltsOfDoomGame.MODID, "entity_second"), () -> new Entity(Entity.Archetype.ENTITYBLOB));
 	}
 
 	// DEBUG
