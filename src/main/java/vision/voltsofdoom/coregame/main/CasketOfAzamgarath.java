b74ff72145bc1dabc82df7a10e8586d122723f5a
GenElectrovise <sanchez.pamelos@outlook.com>
2020-10-02 12:30:14 +0100

    Reformatted using the voltsofdoomparent (google style) formatter

--- src/main/java/vision/voltsofdoom/coregame/main/CasketOfAzamgarath.java
+++ src/main/java/vision/voltsofdoom/coregame/main/CasketOfAzamgarath.java
@@ -5,19 +5,19 @@ import vision.voltsofdoom.zapbyte.loading.mod.Mod;
 
 @Mod(modid = CasketOfAzamgarath.MODID)
 public class CasketOfAzamgarath {
-	public static final String MODID = "casketofazamgarath";
-	private GameRegistry gameRegistry;
-	public static CasketOfAzamgarath instance;
+  public static final String MODID = "casketofazamgarath";
+  private GameRegistry gameRegistry;
+  public static CasketOfAzamgarath instance;
 
-	public CasketOfAzamgarath() {
-		gameRegistry = new GameRegistry();
-	}
+  public CasketOfAzamgarath() {
+    gameRegistry = new GameRegistry();
+  }
 
-	public GameRegistry getGameregistry() {
-		return gameRegistry;
-	}
+  public GameRegistry getGameregistry() {
+    return gameRegistry;
+  }
 
-	public String getModid() {
-		return "vod_coregame";
-	}
+  public String getModid() {
+    return "vod_coregame";
+  }
 }
--- src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
+++ src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
@@ -22,75 +22,92 @@ import vision.voltsofdoom.zapbyte.resource.ResourceLocation;
 @Stowaway
 public class GameRegistry {
 
-	public static RegistryType<Tile> TILE_SECOND_TYPE;
+  public static RegistryType<Tile> TILE_SECOND_TYPE;
 
-	public static TypeRegistry<Tile> TILE_SECOND;
-	public static TypeRegistry<Entity> ENTITY_SECOND_TO_BE_MERGED;
-	public static TypeRegistry<Tile> TILES;
-	public static TypeRegistry<Entity> ENTITIES;
+  public static TypeRegistry<Tile> TILE_SECOND;
+  public static TypeRegistry<Entity> ENTITY_SECOND_TO_BE_MERGED;
+  public static TypeRegistry<Tile> TILES;
+  public static TypeRegistry<Entity> ENTITIES;
 
-	public static RegistryMessenger<Tile> test_tile;
-	public static RegistryMessenger<Tile> test_tile_second;
-	public static RegistryMessenger<Entity> test_entity;
-	public static RegistryMessenger<Entity> test_entity_merged;
+  public static RegistryMessenger<Tile> test_tile;
+  public static RegistryMessenger<Tile> test_tile_second;
+  public static RegistryMessenger<Entity> test_entity;
+  public static RegistryMessenger<Entity> test_entity_merged;
 
-	public GameRegistry() {
-		Loggers.MOD.info("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
-	}
+  public GameRegistry() {
+    Loggers.MOD.info(
+        "Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
+  }
 
-	@Stowaway
-	private static void createRegistryTypes(RegistryEvent.CreateRegistryTypesEvent event) {
-		Loggers.MOD_DETAIL.fine("Creating RegistryTypes");
-		TILE_SECOND_TYPE = event.createRegistryType(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), Tile.class);
-	}
+  @Stowaway
+  private static void createRegistryTypes(RegistryEvent.CreateRegistryTypesEvent event) {
+    Loggers.MOD_DETAIL.fine("Creating RegistryTypes");
+    TILE_SECOND_TYPE = event.createRegistryType(
+        new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), Tile.class);
+  }
 
-	@Stowaway
-	private static void createAndSubmitTypeRegistries(RegistryEvent.CreateAndSubmitRegistriesEvent event) {
-		Loggers.MOD_DETAIL.fine("Creating and submitting TypeRegistries");
-		TILES = new TypeRegistry<Tile>(new ResourceLocation(CasketOfAzamgarath.MODID, "tiles"), RegistryTypes.TILES);
-		ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(CasketOfAzamgarath.MODID, "entities"), RegistryTypes.ENTITIES);
-		TILE_SECOND = new TypeRegistry<Tile>(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), TILE_SECOND_TYPE);
-		ENTITY_SECOND_TO_BE_MERGED = new TypeRegistry<Entity>(new ResourceLocation(CasketOfAzamgarath.MODID, "entity_second_to_be_merged"), RegistryTypes.ENTITIES);
-		event.submit(TILES);
-		event.submit(ENTITIES);
-		event.submit(TILE_SECOND);
-		event.submit(ENTITY_SECOND_TO_BE_MERGED);
-	}
+  @Stowaway
+  private static void createAndSubmitTypeRegistries(
+      RegistryEvent.CreateAndSubmitRegistriesEvent event) {
+    Loggers.MOD_DETAIL.fine("Creating and submitting TypeRegistries");
+    TILES = new TypeRegistry<Tile>(new ResourceLocation(CasketOfAzamgarath.MODID, "tiles"),
+        RegistryTypes.TILES);
+    ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(CasketOfAzamgarath.MODID, "entities"),
+        RegistryTypes.ENTITIES);
+    TILE_SECOND = new TypeRegistry<Tile>(
+        new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), TILE_SECOND_TYPE);
+    ENTITY_SECOND_TO_BE_MERGED = new TypeRegistry<Entity>(
+        new ResourceLocation(CasketOfAzamgarath.MODID, "entity_second_to_be_merged"),
+        RegistryTypes.ENTITIES);
+    event.submit(TILES);
+    event.submit(ENTITIES);
+    event.submit(TILE_SECOND);
+    event.submit(ENTITY_SECOND_TO_BE_MERGED);
+  }
 
-	@Stowaway
-	private static void populateTypeRegistriesEventListener(RegistryEvent.PopulateTypeRegistriesEvent event) {
-		Loggers.MOD_DETAIL.fine("Populating TypeRegistries");
-		test_tile = TILES.register(new ResourceLocation(CasketOfAzamgarath.MODID, "test_tile"), () -> new Tile(Tile.Archetype.COBBLE));
+  @Stowaway
+  private static void populateTypeRegistriesEventListener(
+      RegistryEvent.PopulateTypeRegistriesEvent event) {
+    Loggers.MOD_DETAIL.fine("Populating TypeRegistries");
+    test_tile = TILES.register(new ResourceLocation(CasketOfAzamgarath.MODID, "test_tile"),
+        () -> new Tile(Tile.Archetype.COBBLE));
 
-		test_tile_second = TILE_SECOND.register(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), () -> new Tile(Tile.Archetype.WOODLOG));
+    test_tile_second =
+        TILE_SECOND.register(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"),
+            () -> new Tile(Tile.Archetype.WOODLOG));
 
-		test_entity = ENTITIES.register(new ResourceLocation(CasketOfAzamgarath.MODID, "entity"), () -> new Entity(Entity.Archetype.ENTITYBLOB));
-		test_entity_merged = ENTITY_SECOND_TO_BE_MERGED.register(new ResourceLocation(CasketOfAzamgarath.MODID, "entity_second"), () -> new Entity(Entity.Archetype.ENTITYBLOB));
-	}
+    test_entity = ENTITIES.register(new ResourceLocation(CasketOfAzamgarath.MODID, "entity"),
+        () -> new Entity(Entity.Archetype.ENTITYBLOB));
+    test_entity_merged = ENTITY_SECOND_TO_BE_MERGED.register(
+        new ResourceLocation(CasketOfAzamgarath.MODID, "entity_second"),
+        () -> new Entity(Entity.Archetype.ENTITYBLOB));
+  }
 
-	@Stowaway
-	private static void loadingDone(RegistryEvent.LoadingDoneEvent event) {
-		System.out.println("Test tile" + test_tile.get());
-		test_tile.get();
-	}
+  @Stowaway
+  private static void loadingDone(RegistryEvent.LoadingDoneEvent event) {
+    System.out.println("Test tile" + test_tile.get());
+    test_tile.get();
+  }
 
-	// DEBUG
+  // DEBUG
 
-	public static void typeStowawayTestObject(Object notAnEvent) {
+  public static void typeStowawayTestObject(Object notAnEvent) {
 
-	}
+  }
 
-	public static void typeStowawayTestNotEvent(String notAnEvent) {
+  public static void typeStowawayTestNotEvent(String notAnEvent) {
 
-	}
+  }
 
-	@Stowaway
-	public static void methodStowawayTest(LoadingEvent.TestEvent event) {
-		Loggers.MOD_DETAIL.fine("Volts of Doom Coregame (GameRegistry#methodStowawayTest) has heard a LoadingEvent.TestEvent Event!");
-	}
+  @Stowaway
+  public static void methodStowawayTest(LoadingEvent.TestEvent event) {
+    Loggers.MOD_DETAIL.fine(
+        "Volts of Doom Coregame (GameRegistry#methodStowawayTest) has heard a LoadingEvent.TestEvent Event!");
+  }
 
-	@Stowaway
-	public static void bandWagonCreationEvent(LoadingEvent.BandWagonCreation event) {
-		Loggers.MOD_DETAIL.fine("Volts of Doom Coregame (GameRegistry#bandWagonCreationEvent) has heard the LoadingEvent.BandWagonCreation Event!");
-	}
+  @Stowaway
+  public static void bandWagonCreationEvent(LoadingEvent.BandWagonCreation event) {
+    Loggers.MOD_DETAIL.fine(
+        "Volts of Doom Coregame (GameRegistry#bandWagonCreationEvent) has heard the LoadingEvent.BandWagonCreation Event!");
+  }
 }
