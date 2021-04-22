ef39e81caf812e73f71243318de31d551bd8a0f3
GenElectrovise <sanchez.pamelos@outlook.com>
2021-04-22 19:28:03 +0100

    Updated to remove old ZB registry

--- src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
+++ src/main/java/vision/voltsofdoom/coregame/registry/GameRegistry.java
@@ -4,17 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import vision.voltsofdoom.coregame.main.CasketOfAzamgarath;
 import vision.voltsofdoom.voltsofdoom.VoltsOfDoom;
-import vision.voltsofdoom.voltsofdoom.entity.Entity;
-import vision.voltsofdoom.voltsofdoom.registry.RegistryTypes;
 import vision.voltsofdoom.voltsofdoom.tile.Tile;
-import vision.voltsofdoom.zapbyte.ZapByte;
-import vision.voltsofdoom.zapbyte.event.LoadingEvent;
-import vision.voltsofdoom.zapbyte.event.RegistryEvent;
-import vision.voltsofdoom.zapbyte.event.Stowaway;
-import vision.voltsofdoom.zapbyte.registry.RegistryMessenger;
-import vision.voltsofdoom.zapbyte.registry.RegistryType;
-import vision.voltsofdoom.zapbyte.registry.TypeRegistry;
-import vision.voltsofdoom.zapbyte.registry2.IRegistryMessenger2;
+import vision.voltsofdoom.zapbyte.bandwagon.Stowaway;
+import vision.voltsofdoom.zapbyte.bandwagon.event.LoadingDoneEvent;
+import vision.voltsofdoom.zapbyte.bandwagon.event.LoadingEvent;
+import vision.voltsofdoom.zapbyte.registry.IRegistryMessenger2;
 import vision.voltsofdoom.zapbyte.resource.ResourceLocation;
 
 /**
@@ -28,24 +22,12 @@ public class GameRegistry {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GameRegistry.class);
 
-  public static RegistryType<Tile> TILE_SECOND_TYPE;
-
-  public static TypeRegistry<Tile> TILE_SECOND;
-  public static TypeRegistry<Entity> ENTITY_SECOND_TO_BE_MERGED;
-  public static TypeRegistry<Tile> TILES;
-  public static TypeRegistry<Entity> ENTITIES;
-
-  public static RegistryMessenger<Tile> casket_of_azamgarath_test_tile;
-  public static RegistryMessenger<Tile> test_tile_second;
-  public static RegistryMessenger<Entity> test_entity;
-  public static RegistryMessenger<Entity> test_entity_merged;
-  public static RegistryMessenger<Tile> coresystem_example_tile;
-
   public GameRegistry() {
     CasketOfAzamgarath.LOGGER.info("Found Volts Of Doom (coregame) GameRegistry successfully. Let's load some registries!");
 
     IRegistryMessenger2<Tile> messenger = VoltsOfDoom.getInstance().getRegistry().register(new ResourceLocation("temp", "temp_e"), () -> new Tile(), Tile.class);
     
+    @SuppressWarnings("unused")
     Tile tile = messenger.get();
     
     System.out.println("210421 : " + messenger.get());
@@ -53,44 +35,8 @@ public class GameRegistry {
   }
 
   @Stowaway
-  private static void createRegistryTypes(RegistryEvent.CreateRegistryTypesEvent event) {
-    CasketOfAzamgarath.LOGGER.debug("Creating RegistryTypes");
-    TILE_SECOND_TYPE = event.createRegistryType(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), Tile.class);
-  }
-
-  @Stowaway
-  private static void createAndSubmitTypeRegistries(RegistryEvent.CreateAndSubmitRegistriesEvent event) {
-    CasketOfAzamgarath.LOGGER.debug("Creating and submitting TypeRegistries");
-    TILES = new TypeRegistry<Tile>(new ResourceLocation(CasketOfAzamgarath.MODID, "tiles"), RegistryTypes.TILES);
-    ENTITIES = new TypeRegistry<Entity>(new ResourceLocation(CasketOfAzamgarath.MODID, "entities"), RegistryTypes.ENTITIES);
-    TILE_SECOND = new TypeRegistry<Tile>(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), TILE_SECOND_TYPE);
-    ENTITY_SECOND_TO_BE_MERGED = new TypeRegistry<Entity>(new ResourceLocation(CasketOfAzamgarath.MODID, "entity_second_to_be_merged"), RegistryTypes.ENTITIES);
-    event.submit(TILES);
-    event.submit(ENTITIES);
-    event.submit(TILE_SECOND);
-    event.submit(ENTITY_SECOND_TO_BE_MERGED);
-  }
-
-  @Stowaway
-  private static void populateTypeRegistriesEventListener(RegistryEvent.PopulateTypeRegistriesEvent event) {
-    CasketOfAzamgarath.LOGGER.debug("Populating TypeRegistries");
-
-    // Tiles
-    casket_of_azamgarath_test_tile = TILES.register(new ResourceLocation(CasketOfAzamgarath.MODID, "test_tile"), () -> new Tile());
-
-    coresystem_example_tile = TILES.register(new ResourceLocation("coresystem", "example_tile"), () -> new Tile());
-
-    test_tile_second = TILE_SECOND.register(new ResourceLocation(CasketOfAzamgarath.MODID, "tile_second"), () -> new Tile());
-
-    // Entities
-    test_entity = ENTITIES.register(new ResourceLocation(CasketOfAzamgarath.MODID, "entity"), () -> new Entity());
-
-    test_entity_merged = ENTITY_SECOND_TO_BE_MERGED.register(new ResourceLocation(CasketOfAzamgarath.MODID, "entity_second"), () -> new Entity());
-  }
-
-  @Stowaway
-  private static void loadingDone(RegistryEvent.LoadingDoneEvent event) {
-    LOGGER.info("TEST: Test tile (coa_test_tile): " + casket_of_azamgarath_test_tile.get());
+  private static void loadingDone(LoadingDoneEvent event) {
+    LOGGER.info("Casket of Azamgarath Loading Done");
   }
 
   // DEBUG
